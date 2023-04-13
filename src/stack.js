c37de9874c1dca4cdf386d9c9e225e007cd3e0e9
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-04-13 17:29:25 +0300

    feat: add solution

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
+  }
+  push(element) {
+    Array.prototype.unshift.call(this.stack, element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return Array.prototype.shift.call(this.stack);
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[0];
   }
 }
 
